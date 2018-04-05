4d503081ad091e91163e7ffac91b0c5357b94b17
Yuta Suzuki <yuta@Yutas-Macbook-Pro.local>
2018-04-05 13:50:29 +0900

    change

--- sample_shaded_errorbar.R
+++ sample_shaded_errorbar.R
@@ -1,6 +1,9 @@
 
-source("/Users/Yuta-PC/Documents/Rscript/shaded_error/shaded_errorbar.R")
+source("shaded_errorbar.R")
 require("R.matlab")
+library(ggsci)
+library(ggsignif)
+library(ggplot2)
 
 ## example) number of predictor = 2   --------
 ## It needs to make the dataset by matlab as follow:
@@ -9,43 +12,36 @@ require("R.matlab")
 ## PLRData( predictor(NR or R), time, 3) = Standard error of the mean(upper)
 ## PLRData( predictor(NR or R), time, 4) = averaged PLR
 
-PLRdata <- readMat('/Users/Yuta-PC/Documents/matlab/Insight/hiddenPictureAnalysis6.3.0/datashow/data5.mat')
-g <- c("PNN","PNR","PRR")
+PLRdata <- readMat('data1.mat')
+g <- c("PNN","PNR")
 d <- makeDataSet(PLRdata,g)
-config <- list(xlim = c(-0.2, 4.0),
-               ylim = c(-0.5, 0.2),
+config <- list(lim_x = c(-0.2,1.5),
+               lim_y = c(-0.5, 4),
                alpha = 0.3,
                label_x = "Condition",
                label_y = "Pupil Changes [%]",
-               gr = c("#FFFFFF","#808080","#1A1A1A"),
-               gr_point = c("#F8766D","#ECB01F","#619CFF")
+               gr = c("#F8766D","#ECB01F"),
+               grCol = c("#F8766D","#ECB01F")
 )
 
 p <- disp(d,config,1)
 print(p)
 
 
-
 ## example) number of predictor = 3   --------
-# source( "/Users/Yuta-PC/Documents/Rscript/clearFunc.R" )
-# clearAll()
-# 
-# source("/Users/Yuta-PC/Documents/Rscript/shaded_error/shaded_errorbar.R")
-# require("R.matlab")
-# library(ggsci)
-# 
-# PLRdata <- readMat('/Users/Yuta-PC/Documents/matlab/Insight/hiddenPictureAnalysis6.3.0/datashow/data5.mat')
-# g <- c("NR->R","NR->NR",'R->R')
-# d <- makeDataSet(PLRdata,g)
-# 
-# config <- list(lim_x = c(-0.2, 3),
-#                lim_y = c(-0.5, 8),
-#                alpha = 0.3,
-#                label_x = "Time [s]",
-#                label_y = "Pupil Changes [%]",
-#                gr = c("#ECB01F","#F8766D","#619CFF"),
-#                gr_point = c("#F8766D","#ECB01F","#619CFF")
-# )
-# p <- disp(d,config,1)
-# print(p)
+
+PLRdata <- readMat('data2.mat')
+g <- c("NR->R","NR->NR",'R->R')
+d <- makeDataSet(PLRdata,g)
+
+config <- list(lim_x = c(-0.2, 1.5),
+               lim_y = c(-0.5, 4),
+               alpha = 0.3,
+               label_x = "Time [s]",
+               label_y = "Pupil Changes [%]",
+               gr = c("#ECB01F","#F8766D","#619CFF"),
+               grCol = c("#F8766D","#ECB01F","#619CFF")
+)
+p <- disp(d,config,1)
+print(p)
 ##------------------------------------------
